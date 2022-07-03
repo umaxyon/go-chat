eb9a702ce7732bb10b37c2b4211833062ef88c5b
Umaxyon <umaxyon@gmail.com>
2022-07-03 22:56:35 +0900

    atomに型追加

--- front/state/atoms.ts
+++ front/state/atoms.ts
@@ -1,11 +1,22 @@
 import { atom } from "recoil"
 
-export const membersState = atom({
+type Member = {
+    user: string
+}
+
+type Comment = {
+    id: string,
+    user: string,
+    text: string,
+    createdAt: any
+}
+
+export const membersState = atom<Member[]>({
     key: "members",
     default: [],
 })
 
-export const commentsState = atom({
+export const commentsState = atom<Comment[]>({
     key: "comments",
     default: [],
 })
