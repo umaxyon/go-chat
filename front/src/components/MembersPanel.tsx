48f86fc947b9ecffe2934e3aaf5917108c2a78b1
Umaxyon <umaxyon@gmail.com>
2022-07-10 20:53:26 +0900

    MembersPanelのスクロール対応

--- front/src/components/MembersPanel.tsx
+++ front/src/components/MembersPanel.tsx
@@ -6,7 +6,7 @@ const MembersPanel = () => {
     const members = useRecoilValue(membersState)
 
     return (
-        <div className="w-full h-full p-1">
+        <div className="w-full h-full p-1 overflow-y-scroll overflow-x-hidden scrollDiv">
             {members.map((m, i) => { return (
                 <div key={`${m.user}_${i}`} className="text-sm">{m.user}</div>
             )})}
