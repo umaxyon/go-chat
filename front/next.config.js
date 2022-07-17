3eb698017223e88d97127b2bebd2ed17555b3d0a
Umaxyon <umaxyon@gmail.com>
2022-07-17 17:16:06 +0900

    next.config.jsにCache-Controlヘッダ追加

--- front/next.config.js
+++ front/next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
+
+const cacheHeaders = [
+  { key: "Cache-Control", value: "max-age=0" },
+]
+
 const nextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
+  async headers() { return [
+    { source: '/*', headers: cacheHeaders }
+  ] },
 }
 
 module.exports = nextConfig
